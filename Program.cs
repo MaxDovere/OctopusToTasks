8443cc9f2fac5eb8cb3b0a08a249c3350b712453
max <max.dovere@fullserv.it>
2021-12-30 21:55:40 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
+using System.Threading.Tasks;
+
+namespace OctopusToTasks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            MonitorLoop monitorLoop = host.Services.GetRequiredService<MonitorLoop>()!;
+            monitorLoop.StartMonitorLoop();
+            
+            host.Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(options => 
+                    options.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Information))
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<MonitorLoop>();
+                    services.AddSingleton<IBackgroundTaskQueue>(_ =>
+                    {
+                        if (!int.TryParse(hostContext.Configuration["QueueCapacity"], out var queueCapacity))
+                        {
+                            queueCapacity = 100;
+                        }
+
+                        return new DefaultBackgroundTaskQueue(queueCapacity);
+                    });
+                    services.AddHostedService<QueuedHostedService>()
+                        .Configure<EventLogSettings>(config =>
+                        {
+                            config.LogName = "OctopusToTasks Service";
+                            config.SourceName = "OctopusToTasks Service Source";
+                        });
+                })
+                .UseWindowsService(config =>
+                    config.ServiceName = "OctopusToTasks Service 1.0"
+                );
+        }
+}
